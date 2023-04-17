18d65bb1e8879f410a57df5e379d164f46a4e5bb
Kitesa <kitesamerga@gmail.com>
2023-04-17 17:39:31 +0300

    Updated password reset code

--- explainit/explainit/urls.py
+++ explainit/explainit/urls.py
@@ -18,12 +18,21 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
-admin.site.site_header = 'XPLAINIT-Admin'
+admin.site.site_header = 'ExplainIT-Admin'
 urlpatterns = [
     path('', include('homepage.urls')),
     path('accounts/', include('accounts.urls')),
+    path('courses/', include('courses.urls')),
+    path('questions/', include('qanda.urls')),
     path('admin/', admin.site.urls),
+
+    #PASSWORD
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password/password_reset.html'), name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_confirm.html'),                                                                                  name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 if settings.DEBUG:
