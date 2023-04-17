b4639b398df010811db6f73f147a48801a4de61f
Kitesa <kitesamerga@gmail.com>
2023-04-17 17:45:53 +0300

    ADded a view controller for home page

--- explainit/homepage/views.py
+++ explainit/homepage/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from qanda.models import Question
+from courses.models import OurCourse
+from accounts.models import Account
+
 
 def HomePageView(request):
+	context = {}
+	land_context = {}
+	all_questions = Question.objects.all()
+	our_courses = OurCourse.objects.all().order_by('date_created')[:6]
+	land_context['our_courses'] = our_courses
+	our_teachers = Account.objects.filter(is_our_teacher=1)
+	context['questions'] = all_questions
 	if request.user.is_authenticated:
-		return render(request, 'homepage/homepage.html')
+		user_image = request.user.profile_pic.image.url
+		context['user_image'] = user_image
+		return render(request, 'homepage/homepage.html', context)
 	else:
-		return render(request, 'homepage/landing_page.html')
+		return render(request, 'homepage/landing_page.html', land_context)
