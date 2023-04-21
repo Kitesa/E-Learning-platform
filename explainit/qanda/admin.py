cd1df15997fd92669415c90a98079b942ddbe27b
firaolw <101394602+firaolw@users.noreply.github.com>
2023-04-21 01:59:41 -0700

    misstake push

--- explainit/qanda/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import (QuestionCategory, 
-					Question,
-					Answer,
-					)
-
-admin.site.register(QuestionCategory)
-admin.site.register(Question)
-admin.site.register(Answer)
